730924307d4bec1b93325c43caab4dbacad922ac
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-06-03 17:57:24 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -15,6 +15,6 @@ def move(board, position, char = "X")
 def update_array_at_with(array, index, value)
   array[index] = value
 end
-update_array_at_with(board, 0, char)
+update_array_at_with(board, 0, value)
 board
 end
