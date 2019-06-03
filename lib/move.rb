8afd851568f86cbb2404f7ed36d137d38d207bb1
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-06-03 18:21:55 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,6 @@ def input_to_index(user_input)
   "#{user_input}".to_i - 1
 end
 board = [" ", " ", " "]
-def update_array_at_with(board, index, char = "X")
-  array[index] = value
+def move(array, index, char = "X")
+  array[index] = "#{char}"
 end
